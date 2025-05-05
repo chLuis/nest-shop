1d81850cefaaa82f05520f8f9ad30b33f1a85131
Luis Chrestia <chrestia.luis@gmail.com>
2025-05-05 12:17:08 -0300

    agregando servicios a products

--- src/products/products.service.ts
+++ src/products/products.service.ts
@@ -1,15 +1,37 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, InternalServerErrorException } from '@nestjs/common';
 import { CreateProductDto } from './dto/create-product.dto';
 import { UpdateProductDto } from './dto/update-product.dto';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { Product } from './entities/product.entity';
 
 @Injectable()
 export class ProductsService {
-  create(createProductDto: CreateProductDto) {
-    return 'This action adds a new product';
+  //
+  constructor(
+    @InjectRepository(Product)
+    private readonly productRepository: Repository<Product>,
+  ) {}
+
+  async create(createProductDto: CreateProductDto) {
+    try {
+      const product = this.productRepository.create(createProductDto); //Aqui se crea
+      await this.productRepository.save(product); // Aqui se guarda
+      return product;
+    } catch (error) {
+      console.log(error);
+      throw new InternalServerErrorException('Error al crear el producto -');
+    }
   }
 
-  findAll() {
-    return `This action returns all products`;
+  async findAll() {
+    try {
+      const products = await this.productRepository.find();
+      return products;
+    } catch (error) {
+      console.log(error);
+      throw new InternalServerErrorException('Error con GET all');
+    }
   }
 
   findOne(id: number) {
